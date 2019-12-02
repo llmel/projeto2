885a633cc5387238bddfdecc0967cf87e631e80c
llmel <contatoleonardomelo@hotmail.com>
2019-12-02 01:29:41 -0300

    Dashboard funcionando dinamicamente, consultando informações do banco

--- site/public/dashboard/js/funcoes_dashboard.js
+++ site/public/dashboard/js/funcoes_dashboard.js
@@ -1,4 +1,5 @@
 username.innerHTML = `${sessionStorage.usuario}`;
+ta_username.innerHTML = `${sessionStorage.usuario}`;
 
 function logout() {
   sessionStorage.clear();
@@ -7,4 +8,359 @@ function logout() {
 
 function zerar_notif() {
 	qtd_notif.style.display = `none`
+}
+
+function buscar_dados () {
+	aguardar();
+	gmuds_andamento();
+	setTimeout(sete_dias, 7000);
+}
+
+function aguardar () {
+	$('#modal_aguardar').modal('show')
+}
+
+function gmuds_andamento () {
+	fetch(`/leituras/andamento/${sessionStorage.empresa}`,{
+		method: "GET"
+	}).then(resposta => {
+		if (resposta.ok) {
+			resposta.json().then(resultado => {
+				div_gmuds_andamento.innerHTML = resultado.gmuds_andamento;
+			})
+		} else {
+			console.log("Deu ruim");
+		}
+		areas_afetadas();
+	})
+}
+
+function areas_afetadas () {
+	fetch(`/leituras/a_afetadas/${sessionStorage.empresa}`,{
+		method: "GET"
+	}).then(resposta => {
+		if (resposta.ok) {
+			resposta.json().then(resultado => {
+				div_areas_afetadas.innerHTML = resultado.areas_afetadas;
+			})
+		} else {
+			console.log("Deu ruim");
+		}
+		equip_parados();
+	})
+}
+
+function equip_parados () {
+	fetch(`/leituras/e_afetados/${sessionStorage.empresa}`,{
+		method: "GET"
+	}).then(resposta => {
+		if (resposta.ok) {
+			resposta.json().then(resultado => {
+				div_equip_parados.innerHTML = resultado.equip_parados;
+			})
+		} else {
+			console.log("Deu ruim");
+		}
+		gmuds_concluidas();
+	})
+}
+
+function gmuds_concluidas () {
+	fetch(`/leituras/concluidas/${sessionStorage.empresa}`,{
+		method: "GET"
+	}).then(resposta => {
+		if (resposta.ok) {
+			resposta.json().then(resultado => {
+				div_gmuds_concluidas.innerHTML = resultado.gmuds_concluidas;
+			})
+		} else {
+			console.log("Deu ruim");
+		}
+	})
+}
+
+function sete_dias () {
+	fetch(`/leituras/sete_dias/${sessionStorage.empresa}`,{
+		method:"GET"
+	}).then(resposta => {
+		if (resposta.ok) {
+			resposta.json().then(resultado => {
+				for (c = 0;c < resultado.length; c++) {
+					tbl_sete_dias.innerHTML += `
+						<tr>
+							<td id="idgmud">${resultado[c].id}</td>
+	                        <td>${resultado[c].area_afetada}</td>
+	                        <td>${resultado[c].motivo}</td>
+	                        <td>${resultado[c].categoria_gmud}</td>
+	                        <td>${resultado[c].data_conclusao}</td>
+	                        <td>
+	                          <div class="btn-group">
+	                            <button onclick="detalhar_gmud()" class="btn btn-success" href="#"><i class="fas fa-search"></i></i></button>
+	                            
+	                          </div>
+	                        </td>
+	                     </tr>
+					`
+				}
+				
+			})
+		} else {
+			console.log("Deu ruim");
+		}
+		mes_a_mes_buscar();
+	})
+}
+
+var qtd = [0,0,0,0,0,0,0,0,0,0,0,0]
+
+function mes_a_mes_buscar () {
+	
+    fetch(`/leituras/mes_a_mes/${sessionStorage.empresa}`,{
+    	method: "GET"
+    }).then(resposta => {
+    	if (resposta.ok) {
+    		resposta.json().then(resultado => {
+    			for(c = 0;c < resultado.length;c++) {
+    				if (resultado[c].meses == 1) {
+    					qtd[0] = resultado[c].qtd_by_month;
+    				} else if (resultado[c].meses == 2) {
+    					qtd[1] = resultado[c].qtd_by_month;
+    				} else if (resultado[c].meses == 3) {
+    					qtd[2] = resultado[c].qtd_by_month;
+    				} else if (resultado[c].meses == 4) {
+    					qtd[3] = resultado[c].qtd_by_month;
+    				} else if (resultado[c].meses == 5) {
+    					qtd[4] = resultado[c].qtd_by_month;
+    				} else if (resultado[c].meses == 6) {
+    					qtd[5] = resultado[c].qtd_by_month;
+    				} else if (resultado[c].meses == 7) {
+    					qtd[6] = resultado[c].qtd_by_month;
+    				} else if (resultado[c].meses == 8) {
+    					qtd[7] = resultado[c].qtd_by_month;
+    				} else if (resultado[c].meses == 9) {
+    					qtd[8] = resultado[c].qtd_by_month;
+    				} else if (resultado[c].meses == 10) {
+    					qtd[9] = resultado[c].qtd_by_month;
+    				} else if (resultado[c].meses == 11) {
+    					qtd[10] = resultado[c].qtd_by_month;
+    				} else if (resultado[c].meses == 12) {
+    					qtd[11] = resultado[c].qtd_by_month;
+    				}
+    			}
+    			console.log(qtd);	
+    		})
+    	} else {
+    		console.log("Deu errooo");
+    	}
+    	setTimeout(daataa, 3000);
+    	mes_area_buscar();
+    })
+}
+
+function daataa () {
+	var dados = {
+		labels: ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"],
+    	datasets: [{
+      		label: "Quantidade",
+      		backgroundColor: ['#B22222','#FF0000','#FF6347' ,'#FF4500','#FFA500','#FFD700','#B22222','#FF0000','#FF6347' ,'#FF4500','#FFA500','#FFD700'],
+      		hoverBackgroundColor: "#C71585",
+     		 borderColor: "#C71585",
+      		data: qtd,
+    	}]}
+    mes_a_mes_plotar(dados);
+}
+
+function mes_a_mes_plotar (dadoss) {
+    var ctx = document.getElementById("myBarChart");
+    window.grafico = new Chart.Bar(ctx, {
+        data: dadoss,
+        
+    // Parte Fixa de configuração do gráfico, não precisa alterar
+        options: {
+    maintainAspectRatio: false,
+    layout: {
+      padding: {
+        left: 10,
+        right: 25,
+        top: 25,
+        bottom: 0
+      }
+    },
+    scales: {
+      xAxes: [{
+        time: {
+          unit: 'month'
+        },
+        gridLines: {
+          display: false,
+          drawBorder: false
+        },
+        ticks: {
+          maxTicksLimit: 12
+        },
+        maxBarThickness: 35,
+      }],
+      yAxes: [{
+        ticks: {
+          min: 0,
+          max: 100,
+          maxTicksLimit: 20,
+          padding: 20,
+          // Include a dollar sign in the ticks
+          callback: function(value, index, values) {
+            return  + number_format(value);
+          }
+        },
+        gridLines: {
+          color: "rgb(234, 236, 244)",
+          zeroLineColor: "rgb(234, 236, 244)",
+          drawBorder: false,
+          borderDash: [2],
+          zeroLineBorderDash: [2]
+        }
+      }],
+    },
+    legend: {
+      display: false
+    },
+    tooltips: {
+      titleMarginBottom: 10,
+      titleFontColor: '#6e707e',
+      titleFontSize: 15,
+      backgroundColor: "rgb(255,255,255)",
+      bodyFontColor: "#858796",
+      borderColor: '#dddfeb',
+      borderWidth: 1,
+      xPadding: 15,
+      yPadding: 15,
+      displayColors: false,
+      caretPadding: 10,
+      callbacks: {
+        label: function(tooltipItem, chart) {
+          var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
+          return datasetLabel + ': ' + number_format(tooltipItem.yLabel) + ' no ano.' ;
+        }
+      }
+    },
+  }
+
+    });
+	// myBarChart.data.datasets[0].data[0] = 11;
+}
+
+function finalizar_aguardar () {
+	$('#modal_aguardar').modal('hide');
+	atualizar_constante();
+}
+
+function atualizar_constante () {
+	setTimeout(runn, 60000);
+}
+
+function runn () {
+	gmuds_andamento();
+	atualizar_constante();
+}
+
+function detalhar_gmud () {
+	modal_detalhes_gmud.innerHTML = `
+		<div class="modal-content">
+	        <div class="modal-header">
+	          <h5 class="modal-title" id="TituloModalCentralizado">Detalhes da GMUD</h5>
+	        </div>
+	        <div id="conteudo_modal"class="modal-body">
+	          <img style="width:50%;margin-left:140px" src="img/aguarde3.gif">
+	        </div>
+	        <div class="modal-footer">
+	          <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+	        </div>
+      	</div>
+      	`;
+    $('#modal_aguardar').modal('show');
+    buscar_gmud_especifica(idgmud.innerHTML);
+}
+
+function buscar_gmud_especifica (id) {
+	fetch(`/leituras/gmud_especifica/${id}`,{
+		method:"GET"
+	}).then(resposta => {
+		if (resposta.ok) {
+			resposta.json().then(resultado => {
+				conteudo_modal.innerHTML = `
+					ID: ${resultado[0].idGmud} <br>
+					Prioridade: ${resultado[0].prioridade} <br>
+					Motivo: ${resultado[0].motivo} <br>
+					Categoria: ${resultado[0].categoria} <br>
+					Data Conclusão: ${resultado[0].data_conclusao} <br>
+					Equipamento: ${resultado[0].descrição} ${" "} ${resultado[0].relevancia}  ${" com IP "} ${resultado[0].ipMaquina}   <br>
+				`
+			})
+		} else {
+			console.log("Errooooo");
+		}
+	})
+}
+
+var labels = [];
+var qtd_by_areas = [];
+
+function mes_area_buscar () {
+	
+    fetch(`/leituras/mes_area/${sessionStorage.empresa}`,{
+    	method: "GET"
+    }).then(resposta => {
+    	if (resposta.ok) {
+    		resposta.json().then(resultado => {
+    			for (c = 0;c < resultado.length;c++) {
+    				labels.push(resultado[c].areas);
+    				qtd_by_areas.push(resultado[c].qtd_by_area);
+    			}
+    			console.log(labels);
+    			console.log(qtd_by_areas);
+    		})
+    	} else {
+    		console.log("Deu errooo");
+    	}
+    	setTimeout(dados_mes_area, 3000);
+    	setTimeout(finalizar_aguardar, 3000);
+    })
+}
+
+function dados_mes_area () {
+	var dados = {
+    labels: labels,
+    datasets: [{
+      data: qtd_by_areas,
+      backgroundColor: ['#006400','#32CD32','#7CFC00' ,'#2E8B57','#808000'],
+      hoverBackgroundColor: "#C71585",
+      hoverBorderColor: "rgba(234, 236, 244, 1)",
+    }]}
+    mes_area_plotar(dados);
+}
+
+function mes_area_plotar (dadoss) {
+    var ctx = document.getElementById("myPieChart");
+    window.grafico = new Chart (ctx, {
+        type: 'doughnut',
+        data: dadoss,
+        
+    // Parte Fixa de configuração do gráfico, não precisa alterar
+        options: {
+	    maintainAspectRatio: false,
+	    tooltips: {
+	      backgroundColor: "rgb(255,255,255)",
+	      bodyFontColor: "#858796",
+	      borderColor: '#dddfeb',
+	      borderWidth: 1,
+	      xPadding: 15,
+	      yPadding: 15,
+	      displayColors: false,
+	      caretPadding: 10,
+	    },
+	    legend: {
+	      display: false
+	    },
+	    cutoutPercentage: 80,
+	  	},
+	});
 }
--- site/routes/leituras_graficos.js
+++ site/routes/leituras_graficos.js
@@ -4,101 +4,182 @@ var router = express.Router();
 var banco = require('../app-banco');
 // não mexa nessas 3 linhas!
 
-router.get('/ultimas', function (req, res, next) {
-  console.log(banco.conexao);
-  banco.conectar().then(() => {
-    var limite_linhas = 8;
-    return banco.sql.query(`select top ${limite_linhas} 
-                            temperatura, 
-                            umidade, 
-                            FORMAT(momento,'HH:mm:ss') as momento 
-                            from leitura order by id desc`);
-  }).then(consulta => {
+// GMUDS EM ANDAMENTO
+router.get("/andamento/:empresa",function (requisicao,resposta) {
+    console.log("Chegou no endpoint de andamento");
+    banco.conectar().then(() => {
+      return banco.sql.query(`
+          select count (fkstatus) as gmuds_andamento
+          from gmud g
+          join equipamento e on g.fkequipamento = e.idequipamento
+          where g.fkstatus = 2
+          and e.fkempresa = ${requisicao.params.empresa}
+        `).then(consulta => {
+          console.log(consulta);
+          resposta.send(consulta.recordset[0]);
+        }).finally(() => {
+          banco.sql.close();
+        })
+    })
+})
 
-    console.log(`Resultado da consulta: ${JSON.stringify(consulta.recordset)}`);
-    res.send(consulta.recordset);
+// AREAS AFETADAS PELAS GMUDS EM ANDAMENTO
+router.get("/a_afetadas/:empresa",function (requisicao,resposta) {
+    console.log("Chegou no endpoint de afetadas");
+    banco.conectar().then(() => {
+      return banco.sql.query(`
+          select count(distinct a.fkareaafetada) as areas_afetadas
+          from gmud g
+          join equipamento e on g.fkequipamento = e.idequipamento
+          join afeta a on e.fkempresa = a.fkempresaafeta
+          where g.fkstatus = 2
+          and e.fkempresa = ${requisicao.params.empresa}
+        `).then(consulta => {
+          console.log(consulta);
+          resposta.send(consulta.recordset[0]);
+        }).finally(() => {
+          banco.sql.close();
+        })
+    })
+})
 
-  }).catch(err => {
+// EQUIPAMENTOS AFETADOS PELAS GMUDS EM ANDAMENTO
+router.get("/e_afetados/:empresa",function (requisicao,resposta) {
+    console.log("Chegou no endpoint de parados");
+    banco.conectar().then(() => {
+      return banco.sql.query(`
+          select count(e.idequipamento) as equip_parados
+          from afeta a
+          join equipamento e on a.fkareaafeta = e.fkareas
+          where e.fkareas in (
+              select distinct a.fkareaafetada
+              from equipamento e
+              join gmud g on g.fkequipamento = e.idequipamento
+              join afeta a on a.fkareaafeta = e.fkareas
+              where g.fkstatus = 2
+              and e.fkempresa = ${requisicao.params.empresa}
+          )
+          and e.fkempresa in (
+              select distinct a.fkempresaafetada
+              from equipamento e
+              join gmud g on g.fkequipamento = e.idequipamento
+              join afeta a on a.fkareaafeta = e.fkareas
+              where g.fkstatus = 2
+              and e.fkempresa = ${requisicao.params.empresa}
+          )
+        `).then(consulta => {
+          console.log(consulta);
+          resposta.send(consulta.recordset[0]);
+        }).finally(() => {
+          banco.sql.close();
+        })
+    })
+})
 
-    var erro = `Erro na leitura dos últimos registros: ${err}`;
-    console.error(erro);
-    res.status(500).send(erro);
+// GMUDS CONCLUIDAS NO DIA ATUAL
+router.get("/concluidas/:empresa",function (requisicao,resposta) {
+    console.log("Chegou no endpoint de concluidas");
+    banco.conectar().then(() => {
+      return banco.sql.query(`
+          select count(fkstatus) as gmuds_concluidas
+          from gmud g
+          join equipamento e on g.fkequipamento = e.idequipamento
+          where g.fkstatus = 3
+          and e.fkempresa = ${requisicao.params.empresa}
+          and MONTH(datahora) = MONTH(GETDATE())
+          and DAY(datahora) = DAY(GETDATE())
+        `).then(consulta => {
+          console.log(consulta);
+          resposta.send(consulta.recordset[0]);
+        }).finally(() => {
+          banco.sql.close();
+        })
+    })
+})
 
-  }).finally(() => {
-    banco.sql.close();
-  });
-
-});
-
-
-router.get('/estatisticas', function (req, res, next) {
-  console.log(banco.conexao);
-
-  var estatisticas = {
-    temp_maxima: 0,
-    temp_minima: 0,
-    temp_media: 0
-  };
-
+// GMUDS CONCLUIDAS NOS ÚLTIMOS 7 DIAS
+router.get("/sete_dias/:empresa",(requisicao,resposta) => {
+  console.log("Chegou no endpoint de sete dias");
   banco.conectar().then(() => {
     return banco.sql.query(`
-        select 
-          max(temperatura) as temp_maxima, 
-          min(temperatura) as temp_minima, 
-          avg(temperatura) as temp_media 
-        from leitura
-        `);
-  }).then(consulta => {
-    estatisticas.temp_maxima = consulta.recordset[0].temp_maxima;
-    estatisticas.temp_minima = consulta.recordset[0].temp_minima;
-    estatisticas.temp_media = consulta.recordset[0].temp_media;
-    console.log(`Estatísticas: ${JSON.stringify(estatisticas)}`);
-    res.send(estatisticas);
-  }).catch(err => {
+      select g.idgmud as id,a.nomearea as area_afetada,g.motivo,g.categoria as categoria_gmud,FORMAT(datahora,'dd/MM/yyyy') as data_conclusao
+      from gmud g
+      join equipamento e on g.fkequipamento = e.idequipamento
+      join areas a on e.fkareas = a.idareas
+      where g.fkstatus = 3
+      and e.fkempresa = ${requisicao.params.empresa}
+      and datahora >= GETDATE()-7 
+      `).then(resultado => {
+        console.log(resultado);
+        resposta.send(resultado.recordset);
+      }).finally(() => {
+        banco.sql.close();
+      })
+  })
+})
 
-    var erro = `Erro na leitura dos últimos registros: ${err}`;
-    console.error(erro);
-    res.status(500).send(erro);
-
-  }).finally(() => {
-    banco.sql.close();
-  });
-
-});
-
-
-router.get('/tempo-real', function (req, res, next) {
-  console.log(banco.conexao);
-
-  var estatisticas = {
-    temperatura: 0,
-    umidade: 0
-  };
-
+// GMUDS REALIZADAS MES A MES
+router.get("/mes_a_mes/:empresa",(requisicao,resposta) => {
+  console.log("Chegou no endpoint de mes_a_mes");
   banco.conectar().then(() => {
     return banco.sql.query(`
-        select top 1 temperatura, umidade from leitura order by id desc
-        `);
-  }).then(consulta => {
+        select MONTH(datahora) as meses,count(MONTH(datahora)) as qtd_by_month
+        from gmud g
+        join equipamento e on e.idequipamento = g.fkequipamento
+        where g.fkstatus = 3
+        and e.fkempresa = ${requisicao.params.empresa}
+        and YEAR(datahora) = YEAR(GETDATE())
+        group by MONTH(datahora)
+      `).then(resultado => {
+        console.log(resultado);
+        resposta.send(resultado.recordset);
+      }).finally(() => {
+        banco.sql.close();
+      })
+  })
+})
 
-    estatisticas.temperatura = consulta.recordset[0].temperatura;
-    estatisticas.umidade = consulta.recordset[0].umidade;
-    console.log(`Tempo real: ${JSON.stringify(estatisticas)}`);
+// GMUDS POR AREA NO ULTIMO MES
+router.get("/mes_area/:empresa",(requisicao,resposta) => {
+  console.log("Chegou no endpoint de mes_area");
+  banco.conectar().then(() => {
+    return banco.sql.query(`
+        select a.nomearea as areas,count(fkareas) as qtd_by_area
+        from gmud g
+        join equipamento e on e.idequipamento = g.fkequipamento
+        join areas a on e.fkareas = a.idareas
+        where MONTH(g.datahora) = MONTH(GETDATE())-1
+        and e.fkempresa = ${requisicao.params.empresa}
+        group by a.nomearea
+      `).then(resultado => {
+        console.log(resultado);
+        resposta.send(resultado.recordset);
+      }).finally(() => {
+        banco.sql.close();
+      })
+  })
+})
 
-    res.send(estatisticas);
+// BUSCAR DETALHES DE GMUD ESPECÍFICA
+router.get(`/gmud_especifica/:id`,(requisicao,resposta) => {
+  console.log("Chegou no endpoint de gmud gmud_especifica");
+  banco.conectar().then(() => {
+    return banco.sql.query(`
+        select *,FORMAT(g.datahora,'dd/MM/yyyy') as data_conclusao
+        from gmud g
+        join equipamento e on g.fkequipamento = e.idequipamento
+        where idgmud = ${requisicao.params.id}
+      `).then(resultado => {
+        console.log(resultado);
+        resposta.send(resultado.recordset);
+      }).finally(() => {
+        banco.sql.close();
+      })
+  })
+})
 
-  }).catch(err => {
+// QUAIS GMUDS EM ANDAMENTO
 
-    var erro = `Erro na leitura dos registros de tempo real: ${err}`;
-    console.error(erro);
-    res.status(500).send(erro);
-
-  }).finally(() => {
-    banco.sql.close();
-  });
-});
-
-router.get('')
 
 
 
